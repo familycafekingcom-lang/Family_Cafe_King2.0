d15ba3468c7be4e5aa749fcf6d9a354ab823d1f0
familycafekingcom-lang <familycafeking.com@gmail.com>
2026-08-18 12:18:08 +0530

    Update data.ts

--- src/data.ts
+++ src/data.ts
@@ -19,7 +19,7 @@ import {
 export const CONTACT_PHONE = "+91 9170763612";
 export const WHATSAPP_NUMBER = "919170763612";
 export const CONTACT_EMAIL = "familycafeking.com@gmail.com";
-export const WHATSAPP_NUMBER = "919341127991";
+export const WHATSAPP_NUMBER = "919170763612";
 export const LOCATION = "SA 7/140 PS, Benipur Shivpur, Sarnath, Varanasi, 221007";
 
 export const SOCIAL_LINKS = {
